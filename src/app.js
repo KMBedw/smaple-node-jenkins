167133b6d42ee955a48aeb11818a7135975066c3
KMBedw <edwin3@live.fr>
2024-11-28 16:23:22 +0100

    test jenkinsfile 01

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
-
-console.log("Hello world ffcfkkf")
+const http = require('http');
+const port = 3000;
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World!\n');
+});
+server.listen(port, () => {
+    console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+});
